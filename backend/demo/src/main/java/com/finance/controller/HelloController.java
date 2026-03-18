c52fc7bffe4cb6c09637aecb98404e2cd5eef074
shaikhayanali04-prog <shaikhayanali04@gmail.com>
2026-03-19 00:16:43 +0530

    running backend first time

---
+++ backend/demo/src/main/java/com/finance/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.finance.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Finance Tracker Backend Running 🚀";
+    }
+}
